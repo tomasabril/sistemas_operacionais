6e8eec6816270a8b711d8aedb41873d7b4debe93
Your Name <you@example.com>
2017-03-24 11:57:13 -0300

    feito, falta ver se ta certo

--- ping-pong-os---projetos/p00/queue.c
+++ ping-pong-os---projetos/p00/queue.c
@@ -1,3 +1,5 @@
+// --- Tomas Abril --- //
+
 #include "queue.h"
 #include <stdio.h>
 
@@ -12,7 +14,7 @@
 void queue_append (queue_t **queue, queue_t *elem){
     if(queue != NULL ){
         if(elem != NULL){
-            if(elem->prev || elem->next){
+            if(elem->prev == NULL && elem->next == NULL){
                 // inicializando lista nula
                 if(*queue == NULL){
                     *queue = elem;
@@ -42,7 +44,46 @@ void queue_append (queue_t **queue, queue_t *elem){
 // Retorno: apontador para o elemento removido, ou NULL se erro
 
 queue_t *queue_remove (queue_t **queue, queue_t *elem) {
-
+    if(queue != NULL){
+        if(*queue != NULL){
+            if(elem != NULL){
+                if(elem->next != NULL && elem->prev != NULL){
+                    queue_t *elemtmp = *queue;
+                    while (elemtmp != elem){
+                        elemtmp = elemtmp->next;
+                        if(elemtmp == *queue){
+                            printf("\nelemento nao esta na fila");
+                            return NULL;
+                        }
+                    }
+                    // retirando o elemento finalmente, meus deus quanto if
+                    // caso unico elemento na fila
+                    if(elem == *queue && elem->next == elem){
+                        *queue = NULL;
+                        elem->next = NULL;
+                        elem->prev = NULL;
+                    }
+                    // caso primeiro da fila
+                    else if(elem == *queue){
+                        *queue = elem->next;
+                        (elem->prev)->next = elem->next;
+                        (elem->next)->prev = elem->prev;
+                        elem->prev = NULL;
+                        elem->next = NULL;
+                    }
+                    // outros casos 
+                    else{
+                        (elem->prev)->next = elem->next;
+                        (elem->next)->prev = elem->prev;
+                        elem->prev = NULL;
+                        elem->next = NULL;
+                    }                    
+                    return elem;
+                }
+            } else printf("\nelemento nao existe");
+        } else printf("\nfila vazia");
+    } else printf("\nfila nao existe");
+    return NULL;
 }
 
 //------------------------------------------------------------------------------
@@ -50,7 +91,17 @@ queue_t *queue_remove (queue_t **queue, queue_t *elem) {
 // Retorno: numero de elementos na fila
 
 int queue_size (queue_t *queue) {
-
+    int i = 0;
+    if(queue != NULL){ //fila existe
+        i++;
+        queue_t *elemtmp = queue;
+        while(elemtmp->next != queue){
+            i++;
+            elemtmp = elemtmp->next;
+        }
+        
+    }
+    return i;
 }
 
 //------------------------------------------------------------------------------
@@ -61,7 +112,14 @@ int queue_size (queue_t *queue) {
 // void print_elem (void *ptr) ; // ptr aponta para o elemento a imprimir
 
 void queue_print (char *name, queue_t *queue, void print_elem (void*) ) {
-
+    if(queue != NULL){ //fila existe
+        queue_t *elemtmp = queue;
+        while(elemtmp->next != queue){
+            print_elem(elemtmp);
+            elemtmp = elemtmp->next;
+        }
+    }
+    
 }
 
 // so pra compilar
